9578f53f6ec39d383624fd26c9728932195dc244
SparklingMarcel <angelbillyguyon@gmail.com>
2022-04-20 10:06:10 +0200

    Updating futur function

--- projet-league-of-stones/src/utils/osef.js
+++ projet-league-of-stones/src/utils/osef.js
@@ -1,9 +1,26 @@
 import CardReact from "../components/CardReact";
-import {attackEnemyCard, pickCard} from "./queries";
+import {attackEnemyCard, pickCard, playCard} from "./queries";
 
 
-const pickOneCard = async () => {
-    return <div className="col mt-3 cardReact"><CardReact data={await pickCard}/></div>
+const pickOneCard = async (numPlayer) => {
+    let data = await pickCard()
+    if (numPlayer === 1 ) {
+        // game.setState({player1_hand = data.player.hand})
+    }
+    else {
+        // game.setState({player2_hand = data.player.hand})
+    }
+
+}
+
+const playOneCard = async (card, numPlayer) => {
+    let data = await playCard(card.id)
+    if (numPlayer === 1 ) {
+        // game.setState({player1_board: data.player.board, player1_hand = data.player.hand})
+    }
+    else {
+        // game.setState({player2_board: data.player.board, player2_hand = data.player.hand})
+    }
 }
 
 
@@ -17,6 +34,6 @@ export const stringifyDeck = (deck) => {
 
 export const attackCard = async (card1, card2) => {
     const data = await attackEnemyCard(card1.id, card2.id)
-    // player1.setState({hp : data.player1.hp, board: data.player1.board})
-    // player2.setState({hp : data.player2.hp, board: data.player2.board})
+    // game.setState({player1_hp : data.player1.hp, player1_board: data.player1.board})
+    // game.setState({player2_hp : data.player2.hp, player2_board: data.player2.board})
 }
