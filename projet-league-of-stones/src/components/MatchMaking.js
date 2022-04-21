532ac297a463ea283b473559bd9dd416ff3cc923
belgaid-ansar <belgaid.ansar.info@gmail.com>
2022-04-21 16:55:54 +0200

    add icones

--- projet-league-of-stones/src/components/MatchMaking.js
+++ projet-league-of-stones/src/components/MatchMaking.js
@@ -3,6 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import "../App.css"
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faKhanda } from "@fortawesome/free-solid-svg-icons"
+
 
 const MatchMaking = () => {
 
@@ -10,100 +14,88 @@ const MatchMaking = () => {
     const [matchmakingId, setmatchmakingId] = useState('');
     const [, set] = useState('');
     
-    {/*const handleSubmit = (event) => {
-        event.preventDefault()
-        event.stopPropagation()
-        fetch('http://localhost:3001/getAll', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({"userId" :userId, "matchmakingId" : matchmakingId})
-        }).then(response => {return response})
-        .then(data => {
-            const status = data.status
-            data = data.json()
-            if (status === 200) {
-                alert('ok')
-            } else {
-                alert('pas ok')
-            }
-        })
-    }
-*/}
+    
     return (
         <div className='container-fluid vh-100'>
             <div className=" justify-content-center">
                 <h4 className="text-white p-4">Liste des joueurs prets à faire une partie</h4>
                 
-                <div class="row">
-                    <div class="col-lg-10 bg-dark mx-auto rounded l">
+                <div className="row">
+                    <div className="col-lg-10 bg-dark mx-auto rounded l">
                                     
-                        <div class="table-responsive">
-                            <table class="table table-dark table-fixed table-hover">
+                        <div className="table-responsive">
+                            <table className="table table-dark table-fixed table-hover">
                                 <thead>
                                     <tr>
-                                        <th scope="col" class="col-4">#</th>
-                                        <th scope="col" class="col-4">Players</th>
-                                        <th scope="col" class="col-4">Informations</th>
+                                        <th scope="col" className="col-3">#</th>
+                                        <th scope="col" className="col-3">Players</th>
+                                        <th scope="col" className="col-3">Informations</th>
+                                        
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td class="col-4">1</td>
-                                        <td class="col-4">Otto</td>
-                                        <td class="col-4">@mdo</td>
+                                        <td className="col-3">1</td>
+                                        <td className="col-3">Otto</td>
+                                        <td className="col-3">@mdo</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
+                                        
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
                                     <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
+                                        <td className="col-3">2</td>
+                                        <td className="col-3">Thornton</td>
+                                        <td className="col-3">@fat</td>
+                                        <td className="col-3"><a><FontAwesomeIcon icon={faKhanda}/></a></td>
                                     </tr>
-                                    <tr>
-                                        <td class="col-4">2</td>
-                                        <td class="col-4">Thornton</td>
-                                        <td class="col-4">@fat</td>
-                                    </tr>
+            
                                 </tbody>
                             </table>
                         </div>
